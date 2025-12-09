6dc2bf810ecbe0aa46702af863b0686a6468e617
oHqdes <121509181+goodCoder1000@users.noreply.github.com>
2025-12-09 17:37:15 +0000

    done 2

---
+++ challenges1.py
@@ -0,0 +1,19 @@
+import time
+import math
+import random
+
+#challenge 1
+print(abs(24 - int(input("give a random number between 1 and 10:"))))
+
+#challenge 4
+list = []
+list2 = []
+for i in range(2):
+    list[i] = int(input("give int: "))
+    
+for i in range(10):
+    list2[i] = input("give int: ")
+
+print(list)
+print(list2)
+
